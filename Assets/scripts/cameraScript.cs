36e1625e8edb1c0e9167bf84c286863613670c17
Nicolas Matter <n.matter@zense.ch>
2017-12-07 12:25:56 +0100

    Camera Lerp and Distance Relative Zoom

--- Assets/scripts/cameraScript.cs
+++ Assets/scripts/cameraScript.cs
@@ -6,43 +6,58 @@ public class cameraScript : MonoBehaviour {
 
 	public GameObject pivot;
 	public float angle;
+	public float distance;
+	private float t;
+	private float q;
 
 	void Update()
 	{
 		if (Input.GetKey (KeyCode.A))
 		{
-			transform.RotateAround (pivot.transform.position,Vector3.down,angle);
+			t += Time.deltaTime;
+			transform.RotateAround (pivot.transform.position,Vector3.down,Mathf.Lerp(0.5f,angle,t));
 		}	
+		if (Input.GetKeyUp (KeyCode.A))
+		{
+			t = 0;
+		}
 		if (Input.GetKey (KeyCode.D))
 		{
-			transform.RotateAround (pivot.transform.position,Vector3.up,angle);
+			t += Time.deltaTime;
+			transform.RotateAround (pivot.transform.position,Vector3.up,Mathf.Lerp(0.5f,angle,t));
 		}	
+		if (Input.GetKeyUp (KeyCode.D))
+		{
+			t = 0;
+		}
 		if (Input.GetKey (KeyCode.W))
 		{
-			transform.RotateAround (pivot.transform.position,Vector3.left,angle);
+			q += Time.deltaTime;
+			transform.RotateAround (pivot.transform.position,Vector3.left,Mathf.Lerp(0.5f,angle,q));
 		}	
+		if (Input.GetKeyUp (KeyCode.W))
+		{
+			q = 0;
+		}
 		if (Input.GetKey (KeyCode.S))
 		{
-			transform.RotateAround (pivot.transform.position,Vector3.right,angle);
+			q += Time.deltaTime;
+			transform.RotateAround (pivot.transform.position,Vector3.right,Mathf.Lerp(0.5f,angle,q));
 		}	
-//		if (Input.GetKey (KeyCode.Q))
-//		{
-//			transform.RotateAround (pivot.transform.position,Vector3.forward,angle);
-//		}	
-//		if (Input.GetKey (KeyCode.E))
-//		{
-//			transform.RotateAround (pivot.transform.position,Vector3.back,angle);
-//		}	
+		if (Input.GetKeyUp (KeyCode.S))
+		{
+			q = 0;
+		}
+
+		distance = Mathf.Clamp (Vector3.Distance (transform.position, pivot.transform.position), 1f,100f)/10f;
 
 		if (Input.GetAxis ("Mouse ScrollWheel")>0f)
 		{
-			Debug.Log (Input.GetAxis ("Mouse ScrollWheel"));
-			transform.position = Vector3.MoveTowards (transform.position, pivot.transform.position, Input.GetAxis ("Mouse ScrollWheel"));
+			transform.position = Vector3.MoveTowards (transform.position, pivot.transform.position, Input.GetAxis ("Mouse ScrollWheel")*distance);
 		}
 		if (Input.GetAxis ("Mouse ScrollWheel")<0f)
 		{
-			Debug.Log (Input.GetAxis ("Mouse ScrollWheel"));
-			transform.position = Vector3.MoveTowards (transform.position, -pivot.transform.position, Input.GetAxis ("Mouse ScrollWheel"));
+			transform.position = Vector3.MoveTowards (transform.position, -pivot.transform.position, Input.GetAxis ("Mouse ScrollWheel")*distance);
 		}
 	}
 }
